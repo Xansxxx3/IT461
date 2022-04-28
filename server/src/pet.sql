034648c0a17fbd1caba18accd7b50ca33f4d2a17
Xansxxx3 <104577324+Xansxxx3@users.noreply.github.com>
2022-04-28 22:48:34 +0800

    Add files via upload

---
+++ server/src/pet.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE pets;
+
+USE pets;
+
+CREATE TABLE dogs(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (name)
+)
+CREATE TABLE cats(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX (name)
+);
